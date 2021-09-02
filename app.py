555d4ba818fb7e9db738d5c0b39b9521b4123867
tamire11 <89584418+tamire11@users.noreply.github.com>
2021-09-02 18:02:37 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red184\green93\blue213;\red42\green45\blue49;\red201\green201\blue201;
+\red136\green185\blue102;\red203\green203\blue203;}
+{\*\expandedcolortbl;;\cssrgb\c77647\c47059\c86667;\cssrgb\c21961\c23137\c25098;\cssrgb\c82745\c82745\c82745;
+\cssrgb\c59608\c76471\c47451;\cssrgb\c83529\c83529\c83529;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl400\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 print\cf4 \strokec4 (\cf5 \strokec5 'Hello, world!'\cf4 \strokec4 )\cf6 \strokec6 \
+}
